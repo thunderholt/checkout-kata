d8f7f22b084351f3ed3e783d909f7cf21e22edbb
Andy Holt <andy.m.holt@gmail.com>
2019-03-27 21:43:38 +0000

    Created test: Checkout_CalculatesCorrectPriceFor_MultipleScans_SingleProduct (Passing)

--- Checkout.UnitTests/CheckoutTests.cs
+++ Checkout.UnitTests/CheckoutTests.cs
@@ -28,5 +28,17 @@ namespace Checkout.UnitTests
 
             Assert.AreEqual(10, checkout.GetTotalPrice());
         }
+
+        [TestMethod]
+        public void Checkout_CalculatesCorrectPriceFor_MultipleScans_SingleProduct()
+        {
+            var checkout = new Checkout(this.productRepository);
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            Assert.AreEqual(30, checkout.GetTotalPrice());
+        }
     }
 }
